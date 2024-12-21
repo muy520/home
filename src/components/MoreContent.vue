9f67e8ed65bcd9f9f88c21462c0e8877d68fcd08
牧羊 <42006467+muy520@users.noreply.github.com>
2024-12-21 16:33:51 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="more-content">
-    <table><tbody><tr><td align="left">性别</td><td align="left">男</td></tr><tr><td align="left">取向</td><td align="left">女</td></tr><tr><td align="left">性格</td><td align="left">i</td></tr><tr><td align="left">最喜欢的网站</td><td align="left">iirose.com</td></tr><tr><td align="left">最喜欢的动漫电影</td><td align="left">你的名字</td></tr><tr><td align="left">国语常听</td><td align="left">许嵩、薛之谦、李荣浩等</td></tr><tr><td align="left">粤语常听</td><td align="left">陈奕迅、谢安琪、郑中基等</td></tr><tr><td align="left">英语常听</td><td align="left">艾薇儿</td></tr></tbody></table>
+    Hi! My name is MuYang, When I'm free, I enjoy video game, watching movies and anime, and listening to music. <br />
+    I also read on useful/useless information from time to time.<br />
+    If you're interested, please checkout my personal <a href="https://blog.ilol.top">blog</a> where I share my ordinary yet joyful life. <br />
+    Feel free to contact me via <a href="mailto:i@ilol.top">Email</a>
   </div>
 </template>
 
